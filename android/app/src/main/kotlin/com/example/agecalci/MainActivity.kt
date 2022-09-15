bb4c28a649a67354595f9f0396f317b6b74ba38a
SameerChaurasia <sameerc.ic.21@nitj.ac.in>
2022-09-15 11:07:19 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/agecalci/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agecalci
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/age_calculator.dart
@@ -0,0 +1,29 @@
+import 'package:age/age.dart' ;
+
+class AgeCalculation {
+  AgeDuration calculateAge(DateTime today, DateTime birthday)
+  {
+    AgeDuration age;
+    age = Age.dateDifference(fromDate: birthday, toDate: today, includeToDate: false);
+    return age;
+  }
+  //Next Birthday
+
+  AgeDuration nextBirthday(DateTime today, DateTime birthday){
+    DateTime temp = DateTime(today.year, birthday.month,birthday.day);
+    DateTime nextBirthdayDate = temp.isBefore(today)
+    ?Age.add(date: temp, duration: AgeDuration(years: 1))
+        :temp;
+    AgeDuration nextBirthdayDuration = Age.dateDifference(fromDate: today, toDate: nextBirthdayDate);
+    return nextBirthdayDuration;
+  }
+//  Next Birthday weekday
+
+    int nextbday(DateTime today, DateTime birthday){
+      DateTime temp = DateTime(today.year, birthday.month,birthday.day);
+      DateTime nextBirthdayDate = temp.isBefore(today)
+          ?Age.add(date: temp, duration: AgeDuration(years: 1))
+          :temp;
+      return nextBirthdayDate.weekday;
+    }
+}
